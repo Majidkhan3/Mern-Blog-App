bd17aba801a1493a6e37cc85017113b745367e65
Majidkhan3 <majidabid017@gmail.com>
2024-06-29 19:13:20 +0500

    navbar functionailites

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 // import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+// import "flowbite/dist/flowbite.css";
 
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import Header from "../src/components/Header";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;

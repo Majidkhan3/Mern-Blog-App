83a7d90e6411870fb830d60db8f68e08946862bf
Majidkhan3 <majidabid017@gmail.com>
2024-07-05 23:13:08 +0500

    Another client

--- src/components/DashProfile.jsx
+++ src/components/DashProfile.jsx
@@ -1,7 +1,8 @@
 import { Button, TextInput } from "flowbite-react";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
-
+import { getStorage } from "firebase/storage";
+import { app } from "../firebase.js";
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
@@ -14,6 +15,27 @@ export default function DashProfile() {
       setImageFileUrl(URL.createObjectURL(file));
     }
   };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  });
+  const uploadImage = async () => {
+    // service firebase.storage {
+    //   match /b/{bucket}/o {
+    //     match /{allPaths=**} {
+    //       allow read;
+    //       allow write: if
+    //       request.resource.size < 2 * 1024 *1024 &&
+    //       request.resource.contentType.matches('image/.*')
+    //     }
+    //   }
+    // }
+
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+  };
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>

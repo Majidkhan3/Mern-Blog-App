575c1e11802113e46e63d0c8558aea9a523902f8
Majidkhan3 <majidabid017@gmail.com>
2024-07-09 21:58:04 +0500

    image uploader

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,8 +1,60 @@
 import { Button, FileInput, Select, TextInput } from "flowbite-react";
+import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 export default function CreatePost() {
+  const [file, setFile] = useState(null);
+  const [imageUploadProgress, setimageUploadProgress] = useState(null);
+  const [imageUploadError, setimageUploadError] = useState(null);
+  const [formData, setFormData] = useState({});
+
+  const handleUploadImage = async () => {
+    try {
+      if (!file) {
+        setimageUploadError("Please Select an Image");
+        return;
+      }
+      setimageUploadError(null);
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + "-" + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+          setimageUploadProgress(progress.toFixed(0));
+        },
+        (error) => {
+          setimageUploadError("Image Upload Null");
+          setimageUploadProgress(null);
+          // setImageFile(null);
+          // setImageFileUrl(null);
+          // setImageFileUploading(false);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setimageUploadProgress(null);
+            setFormData({ ...formData, image: downloadURL });
+            setimageUploadError(null);
+          });
+        }
+      );
+    } catch (error) {
+      setimageUploadError("Image upload Failed");
+      setimageUploadProgress(null);
+      console.log(error);
+    }
+  };
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
@@ -23,12 +75,17 @@ export default function CreatePost() {
           </Select>
         </div>
         <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
-          <FileInput type="file" accept="image/*" />
+          <FileInput
+            type="file"
+            accept="image/*"
+            onChange={(e) => setFile(e.target.value(0))}
+          />
           <Button
             type="button"
             gradientDuoTone="purpleToBlue"
             size="sm"
             outline
+            onClick={handleUploadImage}
           >
             Upload image
           </Button>

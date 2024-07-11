d3bf31adc09433d25efc85e4bb08482ac4f47cfe
Majidkhan3 <majidabid017@gmail.com>
2024-07-11 21:55:12 +0500

    fetching

--- src/components/DashPosts.jsx
+++ src/components/DashPosts.jsx
@@ -1,5 +1,96 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Table } from "flowbite-react";
 
 export default function DashPosts() {
-  return <div>Dashboard</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserPosts = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+        } else {
+          console.error(
+            `Failed to fetch posts: ${res.status} - ${data.message}`
+          );
+        }
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (currentUser.isAdmin) {
+      fetchUserPosts();
+    }
+  }, [currentUser._id, currentUser.isAdmin]);
+
+  return (
+    <div className="w-full table-auto overflow-x-scroll md:mx-auto p-3 scrollbar">
+      {currentUser.isAdmin && userPosts.length > 0 && (
+        <>
+          <Table hoverable className="shadow-md">
+            <Table.Head>
+              <Table.HeadCell>Date Updated</Table.HeadCell>
+              <Table.HeadCell>Post image</Table.HeadCell>
+              <Table.HeadCell>Post title</Table.HeadCell>
+              <Table.HeadCell>Category</Table.HeadCell>
+              <Table.HeadCell>Delete</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Edit</span>
+              </Table.HeadCell>
+            </Table.Head>
+            {userPosts.map((post) => (
+              <Table.Body key={post._id} className="divide-y">
+                <Table.Row className="bg-white dark:border-gray-800 dark:bg-gray-800">
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.image}
+                        className="w-20 h-10 object-cover bg-gray-500"
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className="font-medium text-gray-500 dark:text-white"
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>{post.category} </Table.Cell>
+                  <Table.Cell>
+                    <span className="font-medium text-red-500 hover:underline cursor-pointer">
+                      Delete
+                    </span>{" "}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      to={`/update-post/${post._id}`}
+                      className="text-teal-500"
+                    >
+                      <span> Edit</span>{" "}
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+        </>
+      )}
+    </div>
+  );
 }

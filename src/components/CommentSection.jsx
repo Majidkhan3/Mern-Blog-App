e307b84502113d2ad20e337405db1cc95a2e4717
Majidkhan3 <majidabid017@gmail.com>
2024-07-16 20:54:51 +0500

    edit

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -54,6 +54,7 @@ export default function CommentSection({ postId }) {
     };
     getComments();
   }, [postId]);
+  const handleLike = async () => {};
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -117,7 +118,7 @@ export default function CommentSection({ postId }) {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} />
+            <Comment key={comment._id} comment={comment} onLike={handleLike} />
           ))}
         </>
       )}
